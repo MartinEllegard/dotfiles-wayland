03497191291956671a187404a0c7bf403f5638eb
Martin Ellegård <martin.ellegard@gmail.com>
2024-02-12 16:22:49 +0100

    added fish config nvchad nvim

---
+++ fish/config.fish
@@ -0,0 +1,66 @@
+# Remove intro message
+set fish_greeting
+
+### Some Default values
+set TERM "xterm-256color"
+set EDITOR "lvim"
+set -x MANPAGER "lvim -c 'set ft=man'"
+###
+
+### Set keybindings to vi
+function fish_user_key_bindings
+    fish_vi_key_bindings
+end
+###
+
+### Set Path
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $fish_user_paths
+###
+
+### Set Theme
+
+###
+
+### Aliases
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Software rename
+alias vim='nvim'
+alias hx='helix'
+
+# Software update
+alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
+alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
+alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs (yay)
+alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs (yay)
+alias parsua='paru -Sua --noconfirm'             # update only AUR pkgs (paru)
+alias parsyu='paru -Syu --noconfirm'             # update standard pkgs and AUR pkgs (paru)
+alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages
+
+# Update mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# switch between shells
+# I do not recommend switching default SHELL from bash.
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+
+###
